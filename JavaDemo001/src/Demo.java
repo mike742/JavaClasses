4eb235064f5f7ded8c1d72746b42023b96edc0ba
MikeZorin <mykhailo.zorin@gmail.com>
2020-11-04 08:28:42 -0600

    First commit

---
+++ JavaDemo001/src/Demo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		/*
+		// TODO Auto-generated method stub
+		System.out.println("Hello Eclipse");
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		String userName = sc.nextLine();
+		
+		System.out.println("Hello " + userName);
+		*/
+		int abs2 = Math.abs(-20); // abs2 = 20
+		
+		System.out.println(abs2);
+	}
+
+}
